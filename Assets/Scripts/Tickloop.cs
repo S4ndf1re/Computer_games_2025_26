7fe7cb36ced71994f642c945bde752d96b2dcbb3
S4ndf1re <jan.kleinmann2000@gmail.com>
2025-10-27 14:11:46 +0100

    added info to tickloop

--- Assets/Scripts/Tickloop.cs
+++ Assets/Scripts/Tickloop.cs
@@ -12,14 +12,16 @@ public class Tickloop : MonoBehaviour
     [Delayed]
     public int bpm = 0;
 
-    [Delayed]
-    public int tick_length = 0;
+    public int tickLength = 0;
     public bool repeat = true;
     public bool finished = false;
-    public int current_idx = 0;
-    public double current_time_seconds = 0.0;
-    public double second_for_beat = 0.0;
-    public const int beats_in_measure = 4;
+    public int currentIdx = 0;
+    public double currentTimeSeconds = 0.0;
+    public double secondsForBeats = 0.0;
+
+    [Delayed]
+    public int beatsInMeasure = 4;
+    public int numberOfBeats = 4;
 
     private List<GameObject> gameObjects = new List<GameObject>();
     private List<List<GameObject>> ticks = new List<List<GameObject>>();
@@ -39,16 +41,17 @@ public class Tickloop : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
+        tickLength = numberOfBeats * beatsInMeasure;
         gameObjects.Clear();
         ticks.Clear();
         obj_delegate_mapping.Clear();
-        current_idx = 0;
-        current_time_seconds = 0.0;
-        second_for_beat = 1.0 / BpmToSeconds(bpm);
+        currentIdx = 0;
+        currentTimeSeconds = 0.0;
+        secondsForBeats = 1.0 / BpmToSeconds(bpm);
         finished = false;
 
         ticks = new List<List<GameObject>>();
-        for (int i = 0; i < tick_length; i++)
+        for (int i = 0; i < tickLength; i++)
         {
             ticks.Add(new List<GameObject>());
         }
@@ -57,21 +60,21 @@ public class Tickloop : MonoBehaviour
     // Use FixedUpdate instead of Update, since it is better for tick stability, when the updates are always the same spacing
     void FixedUpdate()
     {
-        this.current_time_seconds += Time.fixedDeltaTime;
+        this.currentTimeSeconds += Time.fixedDeltaTime;
 
 
-        var last_idx = (this.current_idx - 1 + this.tick_length) % this.tick_length;
+        var last_idx = (this.currentIdx - 1 + this.tickLength) % this.tickLength;
 
         // This is a little buggy, because bpm_as_seconds cant be compared like this
-        while (this.current_time_seconds >= this.second_for_beat)
+        while (this.currentTimeSeconds >= this.secondsForBeats)
         {
 
             if (!finished)
             {
                 // Trigger Event, since we crossed the tick mark. 
-                if (this.current_idx >= 0 && this.current_idx < this.ticks.Count)
+                if (this.currentIdx >= 0 && this.currentIdx < this.ticks.Count)
                 {
-                    foreach (var obj in this.ticks[this.current_idx])
+                    foreach (var obj in this.ticks[this.currentIdx])
                     {
                         if (this.obj_delegate_mapping.ContainsKey(obj))
                         {
@@ -83,19 +86,19 @@ public class Tickloop : MonoBehaviour
                 // Reset counters
                 if (this.repeat)
                 {
-                    this.current_idx = (this.current_idx + 1) % this.ticks.Count;
+                    this.currentIdx = (this.currentIdx + 1) % this.ticks.Count;
                 }
                 else
                 {
-                    this.current_idx = this.current_idx + 1;
-                    if (this.current_idx >= this.ticks.Count)
+                    this.currentIdx = this.currentIdx + 1;
+                    if (this.currentIdx >= this.ticks.Count)
                     {
                         this.finished = true;
                     }
                 }
             }
 
-            this.current_time_seconds -= this.second_for_beat;
+            this.currentTimeSeconds -= this.secondsForBeats;
 
         }
 
