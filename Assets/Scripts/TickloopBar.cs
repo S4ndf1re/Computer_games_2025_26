b51d51e384e9202b68995e978ba190779054c7b6
unknown <marius.braun@hsbi.de>
2025-10-27 13:55:31 +0100

    first basic Ui for displaying the tickloop

---
+++ Assets/Scripts/TickloopBar.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TickloopBar : MonoBehaviour
+{
+    public Transform bar_transform;
+    public List<TickloopMeasure> measures = new List<TickloopMeasure>();
+    public TickloopMeasure tickloop_measure;
+    public float interval = 0.2f;
+    public Tickloop tickloop;
+    public int displayed_measures = 4;
+
+    private float timer;
+
+    void Start()
+    {
+        tickloop = GameObject.Find("Tickloop").GetComponent<Tickloop>();
+        bar_transform = GameObject.Find("TickloopBar").GetComponent<Transform>();
+        // Alle Punkte inaktiv setzen
+        for(int i = 0; i < displayed_measures; i++)
+        {
+            TickloopMeasure measure = Instantiate(tickloop_measure, bar_transform, false);
+            RectTransform rect_transform = measure.GetComponent<RectTransform>();
+            rect_transform.sizeDelta = new Vector2(4*20, 20);
+            measure.InstantiateBeats(Tickloop.beats_in_measure, measure.transform);
+            //measure.transform.SetParent(bar_transform, false);
+            measures.Add(measure);
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            timer = 0f;
+            AnimateBar();
+        }
+    }
+
+    private void AnimateBar()
+    {
+        // Alle inaktiv
+        int current_measure = tickloop.current_idx / Tickloop.beats_in_measure;
+        int current_beat = tickloop.current_idx % Tickloop.beats_in_measure;
+        // Debug.Log(current_measure);
+        // Debug.Log(current_beat);
+        measures[current_measure].HighlightBeat(current_beat);
+
+        int last_beat = current_beat - 1;
+        if (last_beat == -1)
+        {
+            last_beat = 3;
+        }
+        int last_measure;
+        if (last_beat == 3)
+        {
+            last_measure = current_measure - 1;
+            if (last_measure == -1)
+            {
+                last_measure = 3;
+            }
+        }
+        else
+        {
+            last_measure = current_measure;
+        }
+        measures[last_measure].UnhighlightBeat(last_beat);
+    }
+}
