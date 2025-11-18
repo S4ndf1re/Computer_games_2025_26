ff71d23ec434c6326a6635187c85b00ee36abfc4
mbraun6 <marius.braun@hsbi.de>
2025-11-18 21:31:42 +0100

    fixed MoveJump, the movementspeed is determined by gravity so that an end point of the jump can be targeted

--- Assets/Scripts/tickloop_enabled/enemy_behaviour/MoveJump.cs
+++ Assets/Scripts/tickloop_enabled/enemy_behaviour/MoveJump.cs
@@ -13,8 +13,11 @@ public class MoveJump : EnemyAct
     public float maxJumpSpeed;
     public float currentMoveDuration;
     public Vector3 currentMoveDirection;
-    private float currentMoveSpeed;
+    public float currentMoveSpeed;
     public EnemyGroundCheck groundCheck;
+
+    public Vector3 start;
+    public Vector3 end;
 
     void Start()
     {
@@ -36,6 +39,8 @@ public class MoveJump : EnemyAct
         //jump ends when we land
         if (groundCheck.isGrounded(enemy) && playerVelocity.y < 0)
         {
+            end = enemy.transform.position;
+            Debug.Log("end" + end);
             playerVelocity.y = 0f;
             return true;
         }
@@ -50,24 +55,39 @@ public class MoveJump : EnemyAct
             gravity = currentGravity;
             playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
             currentMoveDuration = 0;
-            currentMoveDirection = DetermineWalkDirection(enemy.gameObject, target);
-            currentMoveSpeed = DetermineJumpSpeed(enemy.gameObject, target);
+            currentMoveDirection = DetermineJumpDirection(target);
+            currentMoveSpeed = DetermineJumpSpeed(target);
+            start = enemy.transform.position;
+            Debug.Log("start" + start);
+
             return true;
         }
         return false;
     }
     
-    private Vector3 DetermineWalkDirection(GameObject enemy, GameObject target)
+    private Vector3 DetermineJumpDirection(GameObject target)
     {
-        return (target.transform.position - enemy.transform.position).normalized;
+        Vector3 enemyWithoutY = new Vector3(enemy.transform.position.x, 0, enemy.transform.position.z);
+        Vector3 targetWithoutY = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+        return (targetWithoutY - enemyWithoutY).normalized;
     }
     /// <summary>
     /// Determines the jumpspeed so that the enemy lands on the targets position even when the maxJumpdistance is bigger than the actual distance.
     /// </summary>
-    private float DetermineJumpSpeed(GameObject enemy, GameObject target)
+    private float DetermineJumpSpeed(GameObject target)
     {
-        float currentDistance = (target.transform.position - enemy.transform.position).magnitude;
-        return currentDistance < maxJumpDistance? currentDistance/maxJumpDistance * maxJumpSpeed : maxJumpSpeed;
+        Vector3 enemyWithoutY = new Vector3(enemy.transform.position.x, 0, enemy.transform.position.z);
+        Vector3 targetWithoutY = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+        float currentDistance = (targetWithoutY - enemyWithoutY).magnitude;
+        float totalJumpDuration = DetermineJumpDuration();
+        return currentDistance < maxJumpDistance? currentDistance / totalJumpDuration : maxJumpDistance / totalJumpDuration;
+    }
+
+    private float DetermineJumpDuration()
+    {
+        //freefall formula: h-1/2 * g * t^2 rearranged to t = sqrt(4*h/g) -> -g because our acceleration is negative
+        // 2 times because we jump up first and fall then, it is a parabola
+        return 2*Mathf.Sqrt(2*jumpHeight/-gravity);
     }
 
     public override void OnHit(Hitbox hitbox)
