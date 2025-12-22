7f560a78970b66441a175557fd0702159f428ba8
auhe <adrian.uhe@uni-bielefeld.de>
2025-12-22 15:17:54 +0100

    added health bar prototype

---
+++ Assets/Scripts/ui/HealthBarController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Player player;
+    // Set this to players max health
+    public int maxHealth;
+    public float health;
+    public float healthRatio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxHealth = player.startHealth;
+        health = maxHealth;
+        healthRatio = health / maxHealth * 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Prozentualen anteil ausrechnen
+        health = player.GetHealth();
+        healthRatio = health / maxHealth * 100;
+
+        if(healthSlider.value != healthRatio)
+        {
+            healthSlider.value = healthRatio;
+        }
+    }
+}
